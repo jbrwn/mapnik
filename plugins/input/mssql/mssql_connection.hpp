031622228f75f3779834fb556436b101dab14e81
Joel Brown <joelsbrown1@gmail.com>
2015-04-26 15:24:59 -0600

    more mssql plugin work

---
+++ plugins/input/mssql/mssql_connection.hpp
@@ -0,0 +1,19 @@
+#ifndef MSSQL_CONNECTION_HPP
+#define MSSQL_CONNECTION_HPP
+
+#include <string>
+
+class mssql_connection
+{
+public:
+    mssql_connection(std::string const& connection_string)
+    {
+        
+    }
+    
+    ~mssql_connection() {}
+
+};
+
+
+#endif // MSSQL_CONNECTION_HP
